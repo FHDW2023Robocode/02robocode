407cc4a26fea47609c4c1c94f1ce46e34882c440
Luhminus <luis.rausch@mt-ag.com>
2023-06-17 17:03:49 +0200

    Victory Royale

--- src/main/java/de/fhdw/robocode/ZwanzigerNuggets.java
+++ src/main/java/de/fhdw/robocode/ZwanzigerNuggets.java
@@ -3,11 +3,14 @@ package de.fhdw.robocode;
 import java.awt.Color;
 import robocode.AdvancedRobot;
 import robocode.HitByBulletEvent;
+import robocode.HitWallEvent;
 import robocode.Robot;
+import robocode.Rules;
 import robocode.ScannedRobotEvent;
 
 public class ZwanzigerNuggets extends AdvancedRobot {
-
+    boolean isScanned = false;
+    double angle = 360.0;
     @Override
     public void run() {
 
@@ -16,19 +19,30 @@ public class ZwanzigerNuggets extends AdvancedRobot {
         double radius = 100.0;
         double angle = 360.0;
 
+        setAdjustRadarForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+        setAdjustGunForRobotTurn(true);
 
         while (true) {
-            if(this.getAllEvents().isEmpty()) {
-                turnLeft(5);
-            }
+            turnLeft(5);
+            turnRadarRight(360);
+            ahead(80);
+            turnRight(40);
 //            turnLeft(angle);
 //            fireBullet(1);
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        setTurnLeft(0);
-        fireBullet(2);
+        ahead(80);
+        setTurnRadarRight(getHeading() - getRadarHeading() + e.getBearing());
+        turnGunRight(getRadarHeading() - getGunHeading());
+//        turnRadarLeft(5);
+        if(getGunHeat() < 40 && getEnergy() > 10 ){
+            fire(2);
+
+        }
+
 //        doNothing();
 //        ahead(100);
 //        fire(2);
@@ -36,6 +50,14 @@ public class ZwanzigerNuggets extends AdvancedRobot {
 
     public void onHitByBullet(HitByBulletEvent e) {
 //        turnLeft(90 - e.getBearing());
+        turnRight(Math.random()*200);
+        ahead(100);
+
     }
 
+    @Override
+    public void onHitWall(HitWallEvent event) {
+        turnRight(90);
+        ahead(-100);
+    }
 }
